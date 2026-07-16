51bc3e485aabad492ce9540785526ac2923302d5
Akankshavns <makanksha858@gmail.com>
2026-07-16 21:45:14 +0530

    add some details in working page

--- Script/header.js
+++ Script/header.js
@@ -3,3 +3,10 @@ fetch("../HelperPage/header.html")
     .then(data => {
         document.getElementById("header").innerHTML = data;
     });
+
+
+fetch("../HelperPage/footer.html")
+.then(response => response.text())
+.then(data => {
+    document.getElementById("footer").innerHTML = data;
+});
